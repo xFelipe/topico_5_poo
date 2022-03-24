f29981a8d0d4e37aee0ddceadf6c15f8444483d3
Felipe Gomes Duarte <xfelipegd@gmail.com>
2022-03-24 12:40:53 -0300

    Refatoração

--- src/exercicio_01/Fatura.java
+++ src/exercicio_01/Fatura.java
@@ -1,13 +1,5 @@
 package exercicio_01;
 
-//1. (Deitel Java 3.13) Crie uma classe em Java chamada fatura para uma loja desuprimentos de informática.
-//A classe deve conter quatro variáveis – o número (String),a descrição (String), a quantidade comprada de
-//um item (int) e o preço por item(double). A classe deve ter um construtor e um método get e set para
-//cada variável deinstância. Além disso, forneça um método chamado getTotalFatura que calcula
-//o valorda fatura e depois retorna o valor como um double. Se o valor não for positivo, ele
-//deveser configurado como 0. Se o preço por item não for positivo, ele deve ser configuradocomo 0.0.
-//Escreva um aplicativo de teste chamado FaturaTeste (em outro arquivo) quedemonstra as capacidades
-//da classe Fatura.
 
 
 public class Fatura {
--- src/exercicio_01/Main.java
+++ src/exercicio_01/Main.java
@@ -6,7 +6,9 @@ public class Main {
 		Fatura f = new Fatura("852131202", "Detergente", 5, 2.50);
 		f.setDescricao("Detergente");
 		
-		System.out.println(f.getTotalFatura());
+		System.out.println("A fatura do produto " + f.getDescricao() + " número:" + f.getNumero());
+		System.out.println("com preco: R$:" + f.getPreco() + " e quantidade: " + f.getQnt());
+		System.out.println("é de: R$:" + f.getTotalFatura());
 	}
 
 }
